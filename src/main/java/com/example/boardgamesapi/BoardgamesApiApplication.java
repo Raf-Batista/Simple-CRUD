3a1d346bb04562dfa8da64421dfa4dc54c320835
Raf-Batista <RafaelB2424@gmail.com>
2021-05-07 12:56:00 -0400

    “initializr”

---
+++ src/main/java/com/example/boardgamesapi/BoardgamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgamesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boardgamesapi/BoardgamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardgamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
